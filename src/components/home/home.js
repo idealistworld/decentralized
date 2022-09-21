a670a7d611d936c82e949f7f30936fe4479b0011
Chris <65946592+idealistworld@users.noreply.github.com>
2022-09-21 11:39:20 -0400

    Added home mobile view

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,24 +5,26 @@ import icons from '../../images/home-icons.png';
 
 const Home = () => {
   return (
-    <div id = "one-element" className = "home-background">
-        <div className = "home-middle-container">
-            <div className = "home-middle-content">
-            <div className = "home-left">  
-            <h1 className = "logo">GoodLogoHere</h1>
-            <h1 className = "home-title">Grow. <br></br> Earn.<br></br>Create.</h1>
-            <p className = "home-paragraph">This is some text that is just going to take up space on the page.  this is just something htat is giong here for the puropse of things being here!!</p>
-            <form className = "submit-section">
-                <input className = "signup-form" placeholder='example@gmail.com' type ="text"></input>
-                <input className = "submit-button" type = "submit" value = "Join Waitlist"></input>
-                </form>
+    <div id="one-element" className="home-background">
+      <div className="home-middle-container">
+        <div className="home-middle-content">
+          <h1 className="logo">GoodLogoHere</h1>
+          <div className="writing-logo">
+            <div id="home-left" className="home-left">
+              <h1 className="home-title">Grow. <br></br> Earn.<br></br>Create.</h1>
+              <p className="home-paragraph">This is some text that is just going to take up space on the page.  this is just something htat is giong here for the puropse of things being here!!</p>
+              <form id="submit-section" className="submit-section">
+                <input className="signup-form" placeholder='example@gmail.com' type="text"></input>
+                <input className="submit-button" type="submit" value="Join Waitlist"></input>
+              </form>
             </div>
-            <div className = "home-right">
-            <img className = "home-icons" src = {icons}></img>
+            <div id="home-right" className="home-right">
+              <img className="home-icons" src={icons}></img>
             </div>
-            </div>
+          </div>
         </div>
-        <Navbar></Navbar>
+      </div>
+      <Navbar></Navbar>
     </div>
   );
 }
